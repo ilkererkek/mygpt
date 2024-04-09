ea1421149f3acd803a1e9a7e5ca6798182809eb9
Ilker Erkek <ilkererkek.cs@gmail.com>
2024-04-09 19:14:08 +0300

    added attention heads and transformers

--- mygpt.py
+++ mygpt.py
@@ -3,14 +3,19 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 #Hyperparameters
-batch_size = 32
-block_size = 8
-max_iters = 3000
+batch_size = 64
+block_size = 256
+max_iters = 5000
 eval_interval = 300
-learning_rate = 1e-2
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embed = 32
+n_embed = 384
+n_layer = 6
+n_head = 6
+dropout = 0.2
+
+print(device)
 
 torch.manual_seed(1337)
 
@@ -52,6 +57,67 @@ def estimate_loss():
     model.train()
     return out
 
+class Head(nn.Module):
+    
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embed, head_size, bias = False)
+        self.query = nn.Linear(n_embed, head_size, bias = False)
+        self.value = nn.Linear(n_embed, head_size, bias = False)
+        self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
+    def forward(self, x):
+        B, T, C = x.shape
+        k = self.key(x)
+        q = self.query(x)
+        
+        wei = q @ k.transpose(-2,-1) * C** -0.5
+        wei = wei.masked_fill(self.tril[:T,:T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+        v = self.value(x)
+        out = wei @ v
+        return out
+
+class MultiHeadAttention(nn.Module):
+    
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embed, n_embed)
+        self.dropout = nn.Dropout(dropout)
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.proj(out)
+        out = self.dropout(out)
+        return out
+
+class FeedForward(nn.Module):
+    
+    def __init__(self, n_embed):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embed, n_embed * 4),
+            nn.ReLU(),
+            nn.Linear(n_embed * 4, n_embed),
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
+class Block(nn.Module):
+    
+    def __init__(self, n_embed, n_head) -> None:
+        super().__init__()
+        head_size = n_embed // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embed)
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
+    
 class BigramLanguageModel(nn.Module):
 
     def __init__(self):
@@ -59,14 +125,19 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
+        self.blocks = nn.Sequential( *[Block(n_embed, n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embed)
         self.lm_head = nn.Linear(n_embed, vocab_size)
 
     def forward(self, idx, targets=None):
         B, T = idx.shape
         # idx and targets are both (B,T) tensor of integers
         token_embeddings = self.token_embedding_table(idx) # (B,T,C)
-        position_embeddings = self.position_embedding_table(torch.arange(T, device=device)) # (B,T,C)
+        position_embeddings = self.position_embedding_table(torch.arange(T, device=device)) # (T,C)
+        
         x = token_embeddings + position_embeddings
+        x = self.blocks(x)
+        x = self.ln_f(x)
         logits = self.lm_head(x)
         if targets is None:
             loss = None
@@ -83,6 +154,7 @@ class BigramLanguageModel(nn.Module):
         for _ in range(max_new_tokens):
             
             idx_cond = idx[:, -block_size:]
+            
             # get the predictions
             logits, loss = self(idx_cond)
             # focus only on the last time step
